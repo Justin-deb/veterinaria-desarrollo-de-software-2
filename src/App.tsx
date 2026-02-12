ba05df8281944cb7bba00b6bed2eaa0f2d2d49bf
Freddy-Baldizon <freddybladizon@gmail.com>
2026-02-12 17:19:32 -0600

    Perfil de mascota

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 import MainLayout from './layouts/MainLayout';
 import "./app.css";
 import Home from './components/Home';
+import PetProfile from './layouts/PetProfile';
 
 function App() {
 
   const routes = createBrowserRouter(createRoutesFromElements(
     <Route path='/' element={<MainLayout/>}>
       <Route index element={<Home/>}></Route>
+      <Route path='/pet/:name' element={<PetProfile/>}/>
     </Route>
   ));
 
