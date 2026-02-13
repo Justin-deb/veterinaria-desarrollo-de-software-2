4b512565edf331cb83d20dcb2ccdb8bb3948ca86
Justin Estrada Cruz <justinestradacruz2006@gmail.com>
2026-02-13 15:07:21 -0600

    Arreglado lo de freddy

--- src/services/Pet.service.ts
+++ src/services/Pet.service.ts
@@ -5,16 +5,16 @@ import { getClientByID } from "./Client.service"
 const PETS_URL = "../../public/data/Data.json" 
 
 export async function getPets(clientID:string): Promise<Pet[]> {
-  const client:Client|null = await getClientByID(clientID);
-  if (client) {
+  const client:Client|undefined = await getClientByID(clientID);
+  if (!client) {
     throw new Error(`Can't load data ${PETS_URL}`)
   }
 
-  return client!.petList;
+  return client.petList;
 }
 
-export async function getPetByName(name: string,clientID:string): Promise<Pet | null> {
+export async function getPetByName(name: string,clientID:string): Promise<Pet | undefined> {
   const pets = await getPets(clientID);
-  const found = pets.find(p => p.petName === name)
-  return found ?? null
+  const found:Pet|undefined = pets.find(p => p.petName === name)
+  return found 
 }
